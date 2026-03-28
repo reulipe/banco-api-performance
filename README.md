2dc8dda255d058c13b5ed8893aa21363e1e95d45
reulipe <eng_euripedes@hotmail.com>
2026-03-27 22:07:53 -0300

    Adicionando arquivo inicial com teste de Login

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # banco-api-performance
+
+
+https://grafana.com/docs/k6/latest/get-started/write-your-first-test/
---
+++ tests/login.test.js
@@ -0,0 +1,35 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+    iterations: 50,
+    thresholds:{
+        http_req_duration: ['p(90)<10', 'max<1'], // 90% das requisições devem ser respondidas em até 10ms e o tempo máximo de resposta deve ser menor que 1s
+        http_req_failed: ['rate<0.01'] // A taxa de falhas deve ser menor que 1%
+
+    }
+};
+
+export default function () {
+
+    const url = 'http://localhost:3009/login';
+    const payload = JSON.stringify({
+        username: 'julio.lima',
+        senha: '123456',
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const response = http.post(url, payload, params);
+    
+    check(response, {
+        'Validar que o status é 200': (r) => r.status === 200,
+        'Validar que o token é String': (r) => typeof(r.json().token) === 'string'
+    })
+
+    sleep(1);
+}
