cea8e31b2ff8c06759073e675af7fc35f66356ed
reulipe <eng_euripedes@hotmail.com>
2026-03-28 23:12:20 -0300

    Adicionei VUS e Duration para execucao do teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,9 +2,11 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-    iterations: 50,
+    //iterations: 50, // número total de iterações a serem executadas
+    vus: 10, // número de usuários virtuais
+    duration: '30s', // duração do teste
     thresholds:{
-        http_req_duration: ['p(90)<10', 'max<1'], // 90% das requisições devem ser respondidas em até 10ms e o tempo máximo de resposta deve ser menor que 1s
+        http_req_duration: ['p(90)<3000', 'max<5000'], // 90% das requisições devem ser respondidas em até 10ms e o tempo máximo de resposta deve ser menor que 1s
         http_req_failed: ['rate<0.01'] // A taxa de falhas deve ser menor que 1%
 
     }
