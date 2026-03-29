a8756d8da10bceece9401c391b558ee592f2e2f4
reulipe <eng_euripedes@hotmail.com>
2026-03-28 23:53:32 -0300

    Adicionei fixtures para os dados de entrada do teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'));
 
 export const options = {
     //iterations: 50, // número total de iterações a serem executadas
@@ -10,7 +11,6 @@ export const options = {
         { duration: '20s', target: 10 }, // manter 10 usuários por 20 segundos
         { duration: '5s', target: 0 }, // ramp-down para 0 usuários em 5 segundos
     ],        
-
     thresholds:{
         http_req_duration: ['p(90)<3000', 'max<5000'], // 90% das requisições devem ser respondidas em até 10ms e o tempo máximo de resposta deve ser menor que 1s
         http_req_failed: ['rate<0.01'] // A taxa de falhas deve ser menor que 1%
@@ -21,10 +21,9 @@ export const options = {
 export default function () {
 
     const url = 'http://localhost:3009/login';
-    const payload = JSON.stringify({
-        username: 'julio.lima',
-        senha: '123456',
-    });
+    postLogin.username = "junior.lima";
+    console.log(postLogin);
+    const payload = JSON.stringify(postLogin);
 
     const params = {
         headers: {
